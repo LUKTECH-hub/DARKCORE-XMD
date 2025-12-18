53414a8977af56fd8a6246ea8b659bc95681883f
lukman549 <asumanlukomwa003@gmail.com>
2025-12-18 03:31:04 +0300

    Update index.js

--- pair/index.js
+++ pair/index.js
@@ -1,21 +1,6 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const {
-  default: makeWASocket,
-  useMultiFileAuthState
-} = require("@whiskeysockets/baileys");
-const pino = require("pino");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-const SESSION_DIR = path.join(__dirname, "session");
-
-app.use(express.static(__dirname));
-
 app.get("/pair", async (req, res) => {
-  const number = process.env.NUMBER;
-  if (!number) return res.send("NUMBER env not set");
+  const number = req.query.number;
+  if (!number) return res.json({ error: "Number required" });
 
   const { state, saveCreds } = await useMultiFileAuthState(SESSION_DIR);
 
@@ -28,17 +13,10 @@ app.get("/pair", async (req, res) => {
 
   if (!sock.authState.creds.registered) {
     const code = await sock.requestPairingCode(number);
+    sock.ev.on("creds.update", saveCreds);
     return res.json({ code });
   }
 
-  sock.ev.on("creds.update", saveCreds);
-  res.json({ status: "already paired" });
+  res.json({ status: "Already paired" });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pair.html"));
-});
-
-app.listen(PORT, () => {
-  console.log("DARKCORE-XMD Pair Site running on port", PORT);
-});
